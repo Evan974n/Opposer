7ddf6f1061df3c7f29873a6bcec9dcd691f81e72
Pigeon <pigeonevan6@yahoo.com>
2024-03-28 10:50:03 +0100

    Ajoutez des fichiers projet.

---
+++ TD5 c#/Program.cs
@@ -0,0 +1,11 @@
+﻿int Somme(int i, int j)
+{
+    return i + j;
+}
+
+Console.WriteLine("Saisir un entier");
+int vale = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Saisir un 2eme entier");
+int vale2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Resultat");
+Console.WriteLine(Somme(vale, vale2));
